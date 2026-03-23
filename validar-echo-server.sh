1fbeabb420fe258f380df76dc6c2b1485571d7f7
Martinsosa95 <csosa@fi.uba.ar>
2026-03-23 01:07:44 -0300

    feat: add solution to ej3 - echo server validation via bash script

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MENSAJE = "HolaMundoTP0"
+
+RESPUESTA = $(docker run --rm --network tp0_testing_net busybox sh -c "echo '$MENSAJE' | nc -w 2 server 12345")
+
+if [ "$RESPUESTA" == "$MENSAJE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
