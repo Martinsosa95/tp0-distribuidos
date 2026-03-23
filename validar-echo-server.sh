e072ebed2a16cf9980d0a0b31e30fe5b8fa7bdc2
Martinsosa95 <csosa@fi.uba.ar>
2026-03-23 01:17:39 -0300

    fix: fixing comparation

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ MENSAJE="HolaMundoTP0"
 
 RESPUESTA=$(docker run --rm --network tp0_testing_net busybox sh -c "echo '$MENSAJE' | nc -w 2 server 12345")
 
-if [ "$RESPUESTA" == "$MENSAJE" ]; then
+if [ "$RESPUESTA" = "$MENSAJE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
