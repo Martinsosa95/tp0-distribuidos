ecd99fa1adcd710528aab37841748f5d86a8c217
Martinsosa95 <csosa@fi.uba.ar>
2026-03-24 11:09:55 -0300

    fix: additional commas

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -53,27 +53,27 @@ func (p *Protocolo) EnviarApuesta(apuesta Apuesta) error {
 		byte(l),
 	}
 	log.Infof(
-		"action: send_data_header | result: in_progress "
+		"action: send_data_header | result: in_progress ",
 	)
 	if _, err := p.conn.Write(header); err != nil {
 		return err
 	}
 	log.Infof(
-		"action: send_data_payload | result: in_progress "
+		"action: send_data_payload | result: in_progress ",
 	)
 	if _, err := p.conn.Write(payload); err != nil {
 		return err
 	}
 
 	log.Infof(
-		"Esperando ACK del servidor... | action: wait_for_ack | result: in_progress "
+		"Esperando ACK del servidor... | action: wait_for_ack | result: in_progress ",
 	)
 	ack := make([]byte, 1)
 	if _, err := io.ReadFull(p.conn, ack); err != nil {
 		return err
 	}
 	log.Infof(
-		"ACK recibido del servidor. | action: wait_for_ack | result: success "
+		"ACK recibido del servidor. | action: wait_for_ack | result: success ",
 	)
 	return nil
 }
