60e0562f041ed52c3666bc21c2368a64d6a23b77
Martinsosa95 <csosa@fi.uba.ar>
2026-03-23 21:36:09 -0300

    feat: add protocol implementation and usage for client

--- client/common/client.go
+++ client/common/client.go
@@ -17,6 +17,11 @@ type ClientConfig struct {
 	ServerAddress string
 	LoopAmount    int
 	LoopPeriod    time.Duration
+	Nombre        string
+	Apellido      string
+	Documento     string
+	Nacimiento    string
+	Numero        string
 }
 
 // Client Entity that encapsulates how
@@ -54,36 +59,42 @@ func (c *Client) createClientSocket() error {
 func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
+	apuesta := Apuesta{
+		Agencia:    c.config.ID,
+		Nombre:     c.config.Nombre,
+		Apellido:   c.config.Apellido,
+		Documento:  c.config.Documento,
+		Nacimiento: c.config.Nacimiento,
+		Numero:     c.config.Numero,
+	}
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
 		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
+		protocol, err := Connect(c.config.ServerAddress)
 		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+			log.Errorf(
+				"action: connect | result: fail | client_id: %v | error: %v",
 				c.config.ID,
 				err,
 			)
-			return
+			continue
 		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
+		err = protocol.EnviarApuesta(apuesta)
+		if err != nil {
+			log.Errorf(
+				"action: send_message | result: fail | error: %v",
+				err,
+			)
+		} else {
+			log.Infof(
+				"action: send_message | result: success | dni: %s | numero: %s",
+				c.config.Documento,
+				c.config.Numero,
+			)
+		}
+		// TODO: Modify the send to avoid short-write
+		protocol.Close()
+		
 		time.Sleep(c.config.LoopPeriod)
 
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
---
+++ client/common/protocol.go
@@ -0,0 +1,69 @@
+package common
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+type Apuesta struct {
+	Agencia   string
+	Nombre    string
+	Apellido  string
+	Documento string
+	Nacimiento string
+	Numero    string
+}
+
+type Protocolo struct {
+	conn net.Conn
+}
+
+func Connect(address string) (*Protocolo, error) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to server: %w", err)
+	}
+	return &Protocolo{conn: conn}, nil
+}
+
+func (p *Protocolo) EnviarApuesta(apuesta Apuesta) error {
+
+	payloadStr := fmt.Sprintf("%s|%s|%s|%s|%s|%s",
+		apuesta.Agencia,
+		apuesta.Nombre,
+		apuesta.Apellido,
+		apuesta.Documento,
+		apuesta.Nacimiento,
+		apuesta.Numero,
+	)
+
+	payload := []byte(payloadStr)
+
+	l := uint32(len(payload))
+	header := []byte{
+		byte(l >> 24),
+		byte(l >> 16),
+		byte(l >> 8),
+		byte(l),
+	}
+	
+	if _, err := p.conn.Write(header); err != nil {
+		return err
+	}
+
+	if _, err := p.conn.Write(payload); err != nil {
+		return err
+	}
+
+	ack := make([]byte, 1)
+	if _, err := io.ReadFull(p.conn, ack); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Protocolo) Close() error {
+	return p.conn.Close()
+}
